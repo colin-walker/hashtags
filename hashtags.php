af60e69e4188aefb63f935fac59521984654e6c3
Colin Walker <colin@colinwalker.blog>
2018-01-20 17:39:59 +0000

    Update hashtags.php

--- hashtags.php
+++ hashtags.php
@@ -1,17 +1,41 @@
 <?php
 
-/* Turn off smart quotes so hashtag function works correctly */
- 
-remove_filter('the_content', 'wptexturize');
- 
-/* convert hashtags to clickable links */
- 
-add_filter( 'the_content', 'linked_hashtags' );
- 
+/* Two ways to make hashtags clickable search links. Just add your preferred solution to functions.php
+ *
+ * The first makes the change when publishing the post so changes the actual post content.
+ * The second leaves the post content untouched, performing the conversion when viewing
+
+
+
+
+/* 1. Before saving the post to database */
+
 function linked_hashtags( $content ) {
-    $content = preg_replace('/(?<!:|\s|&|"|\')(\s)#([^\s|^"|^\.<]+)/i', '<span class="hashtag">$1<a href="'.site_url().'?s=%23'.'$2">#$2</a></span>', $content);
- 
+    $content = preg_replace('/(^|\s)#([A-Za-z0-9_]+)/', '<span class="hashtag"><a href="'.site_url().'?s=%23'.'$2">#$2</a></span>', $content); 
+	
   return $content;
 }
+
+add_filter( 'content_save_pre', 'linked_hashtags', 10, 1 );
+
+
+
+
+/* 2. Only at display */
+
+function linked_hashtags( $content ) {
+    $content = preg_replace('/((?<!&|\)|\||\/|[a-z])#(?!\s|#|\*|\$|^[a-z]).*?)([^\s|^"|^\)|^\.<]+)/i', '<span class="hashtag"><a href="'.site_url().'?s=%23'.'$2">#$2</a></span>', $content); 
+	
+  return $content;
+}
+
+add_filter( 'the_content', 'linked_hashtags', 10, 1 );
+
+
+
+
+/* Smart quotes may have to be disabled for the second option to work if the hashtag is enclosed in quote marks  */
+ 
+remove_filter('the_content', 'wptexturize');
 
 ?>
